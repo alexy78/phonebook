de20b031eb9ff9a6a67c5d6c70d7dbe2d83869dc
alexy78 <sashapeshehonova@gmail.com>
2024-05-09 20:52:40 +0300

    contacts added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+10.times do
+  Contact.create!(
+    name: Faker::Name.name,
+    phone: Faker::PhoneNumber.phone_number,
+    email: Faker::Internet.email
+  )
+end
